82fe870dc68d2e942f9a33c71bd31d16e08bfdf4
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-11-28 20:11:18 -0300

    adicioana ordenação

--- app/Http/Controllers/PetController.php
+++ app/Http/Controllers/PetController.php
@@ -5,42 +5,45 @@ namespace App\Http\Controllers;
 use App\Models\Pet;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PetController extends Controller
 {
-   use HttpResponses;
+    use HttpResponses;
 
-   public function index(Request $request){
-    try {
+    public function index(Request $request)
+    {
+        try {
 
-        // pegar os dados que foram enviados via query params
-        $filters = $request->query();
+            // pegar os dados que foram enviados via query params
+            $filters = $request->query();
 
-        // inicializa uma query
-        $pets = Pet::query();
+            // inicializa uma query
+            $pets = Pet::query();
 
-        // verifica se filtro
-        if($request->has('name') && !empty($filters['name'])) {
-            $pets->where('name', 'ilike', '%'.$filters['name'].'%');
+            // verifica se filtro
+            if ($request->has('name') && !empty($filters['name'])) {
+                $pets->where('name', 'ilike', '%' . $filters['name'] . '%');
+            }
+
+            if ($request->has('age') && !empty($filters['age'])) {
+                $pets->where('age', $filters['age']);
+            }
+
+            if ($request->has('size') && !empty($filters['size'])) {
+                $pets->where('size', $filters['size']);
+            }
+
+            if ($request->has('weight') && !empty($filters['weight'])) {
+                $pets->where('weight', $filters['weight']);
+            }
+
+            // retorna o resultado
+            $columnOrder = $request->has('order') && !empty($filters['order']) ?  $filters['order'] : 'name';
+
+            return $pets->orderBy($columnOrder)->get();
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
-
-        if($request->has('age') && !empty($filters['age'])) {
-            $pets->where('age' ,$filters['age']);
-        }
-
-        if($request->has('size') && !empty($filters['size'])) {
-            $pets->where('size', $filters['size']);
-        }
-
-        if($request->has('weight') && !empty($filters['weight'])) {
-            $pets->where('weight', $filters['weight']);
-        }
-
-        // retorna o resultado
-        return $pets->get();
-
-    } catch (\Exception $exception) {
-
     }
-   }
 }
